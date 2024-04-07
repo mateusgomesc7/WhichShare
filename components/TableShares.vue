e6b65cb9682bf4e507e1bd7860716905da4681db
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2024-04-07 14:33:56 -0300

    create DataTable

---
+++ components/TableShares.vue
@@ -0,0 +1,125 @@
+<template>
+    <div class="card">
+        <DataTable :value="products" removableSort tableStyle="min-width: 50rem">
+            <Column field="code" header="Code" sortable style="width: 14%"></Column>
+            <Column field="name" header="Name" sortable style="width: 16%"></Column>
+            <Column field="PL" header="P/L" sortable style="width: 14%"></Column>
+            <Column field="PVP" header="P/PV" sortable style="width: 14%"></Column>
+            <Column field="cagr_lucros_5_anos" header="CAGR LUCROS 5 ANOS" sortable style="width: 14%"></Column>
+            <Column field="margem_liquida" header="MARGEM LÍQUIDA" sortable style="width: 14%"></Column>
+            <Column field="divida_liquida_ebit" header="DÍVIDA LÍQUIDA / EBIT" sortable style="width: 14%"></Column>
+        </DataTable>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+const products = ref();
+
+const ProductService = () => {
+    const getProductsMini = () => {
+        const response = [
+            { 
+                code: 'MGLU3',
+                name: 'Magazine Luiza',
+                PL: -31.78,
+                PVP: 1.33,
+                cagr_lucros_5_anos: '13.08%',
+                margem_liquida: '4.5%',
+                divida_liquida_ebit: 4.5
+            },
+            {
+                code: 'ABEV3',
+                name: 'Ambev',
+                PL: 12.79,
+                PVP: 2.14,
+                cagr_lucros_5_anos: '15.72%',
+                margem_liquida: '18.29%',
+                divida_liquida_ebit: -0.75
+            },
+            {
+                code: 'PETR4',
+                name: 'Petrobas',
+                PL: 3.65,
+                PVP: 1.29,
+                cagr_lucros_5_anos: '25,03%',
+                margem_liquida: '25.53%',
+                divida_liquida_ebit: 1.2
+            },
+            {
+                code: 'HAPV3',
+                name: 'Hapvida',
+                PL: -29.15,
+                PVP: 0.61,
+                cagr_lucros_5_anos: '37.19%',
+                margem_liquida: '-2.7%',
+                divida_liquida_ebit: 6.64
+            },
+            {
+                code: 'B3SA3',
+                name: 'B3',
+                PL: 16.4,
+                PVP: 3.34,
+                cagr_lucros_5_anos: '8.78%',
+                margem_liquida: '41.65%',
+                divida_liquida_ebit: -0.38
+            },
+            {
+                code: 'ITUB4',
+                name: 'Itaú Unibanco',
+                PL: 9.69,
+                PVP: 1.69,
+                cagr_lucros_5_anos: '4.02%',
+                margem_liquida: '10.57%',
+                divida_liquida_ebit: ''
+            },
+            {
+                code: 'COGN3',
+                name: 'Cogna',
+                PL: -8.53,
+                PVP: 0.35,
+                cagr_lucros_5_anos: '',
+                margem_liquida: '-8,48%',
+                divida_liquida_ebit: 8.17
+            },
+            {
+                code: 'BBDC4',
+                name: 'Banco Bradesco',
+                PL: 10.80,
+                PVP: 0.93,
+                cagr_lucros_5_anos: '-8,48%',
+                margem_liquida: '6,37%',
+                divida_liquida_ebit: ''
+            },
+            {
+                code: 'ITSA4',
+                name: 'Itaúsa',
+                PL: 7.66,
+                PVP: 1.24,
+                cagr_lucros_5_anos: '5,75%',
+                margem_liquida: '182,39%',
+                divida_liquida_ebit: 0.19
+            },
+            {
+                code: 'EQTL3',
+                name: 'Equatorial Energia',
+                PL: 17.60,
+                PVP: 1.73,
+                cagr_lucros_5_anos: '1,16%',
+                margem_liquida: '5,06%',
+                divida_liquida_ebit: 4.47
+            },
+        ];
+        return response;
+    };
+
+    return { getProductsMini };
+};
+
+onMounted(() => {
+    products.value = ProductService().getProductsMini();
+});
+
+
+
+</script>
---
+++ pages/info.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="card">
+        <Fieldset legend="P/L" :toggleable="true">
+            <p class="m-0">
+                Benjamin Graham - O P/L máximo que paga em uma empresa é de 15. Superior a 15 quer fizer que a empresa está cara.
+            </p>
+        </Fieldset>
+        <Fieldset legend="P/PV" :toggleable="true">
+            <p class="m-0">
+                Se estiver 1 quer dizer a empresa está sendo negociada no valor justo. Acima de 1 quer dizer que a empresa está cara. Menor que 1 quer dizer que a empresa está barata.
+                Benjamin Graham - Compra a empresa até 1.5. Fórmula: P/L * P/V <= 22.5
+            </p>
+        </Fieldset>
+        <Fieldset legend="CAGR LUCROS 5 ANOS" :toggleable="true">
+            <p class="m-0">
+                Média de crescimento anual dos lucros da empresa nos últimos 5 anos.
+            </p>
+        </Fieldset>
+        <Fieldset legend="MARGEM LÍQUIDA" :toggleable="true">
+            <p class="m-0">
+                Margem líquida é o lucro líquido dividido pela receita líquida. É a porcentagem de lucro que a empresa obteve em relação ao seu faturamento.
+                Não é indicado investir em empresas com margem líquida negativa ou muito baixa (1, 2, 3%).
+            </p>
+        </Fieldset>
+        <Fieldset legend="DÍVIDA LÍQUIDA / EBIT" :toggleable="true">
+            <p class="m-0">
+                Dívida líquida é a diferença entre o total de dívidas e o total de caixa e equivalentes de caixa. O EBIT é o lucro antes dos juros, impostos, depreciação e amortização.
+                Quanto menor o índice, melhor. Empresas com índice negativo são as melhores.
+                Até 4 é considerado bom. Acima de 4 é considerado ruim.
+            </p>
+        </Fieldset>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
